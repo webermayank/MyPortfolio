2fbe9556a19e9588dbaf43d626e72982cb7cf36a
webermayank <111176033+webermayank@users.noreply.github.com>
2025-06-14 23:57:59 +0530

    corrected project

--- src/components/sections/Projects.tsx
+++ src/components/sections/Projects.tsx
@@ -3,13 +3,13 @@ const webhis: string =
   "https://drive.google.com/file/d/1ilEq17QNPAR83-cNNp6POEK-FTei95LC/view";
 
   const laceverse: string = "https://github.com/webermayank/laceverse-code";
-  const Syncpad: string = "https://github.com/webermayank/Sync-Pad_AI";
+const Syncpad: string = "https://mkvprjt.syncpadai.xyz";
 export const ProjectsContent = [
   "My Works:",
   `Open Source Contributions - <a href="${github}" class="text-blue-500 hover:underline">webermayank</a>`,
+  `<a href="${Syncpad}" class=""><span class="text-terminal-linkBlue text-2xl">Syncpad </span></a>: – AI based Text Editor`,
   `<a href="${webhis}" class=""><span class="text-terminal-linkBlue text-2xl">WebHis </span></a>: Chrome Extension for Enhanced URL History Tracking and Search Query Extraction`,
   `<a href="${laceverse}" class=""><span class="text-terminal-linkBlue text-2xl">LaceVerse </span></a>: Metaverse Gaming + Chatting Simulator(UD)`,
-  `<a href="${Syncpad}" class=""><span class="text-terminal-linkBlue text-2xl">Syncpad </span></a>: – AI based Text Editor`,
   "",
   "Type 'cd ..' or '..' or click on home to go back",
 ];
