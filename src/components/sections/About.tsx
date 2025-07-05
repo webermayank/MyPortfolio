7fa9a2f3cba76c6c446fb74df042830a6b6b63b0
webermayank <111176033+webermayank@users.noreply.github.com>
2025-07-05 13:11:38 +0530

    added skills

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -3,7 +3,7 @@ export const AboutContent = [
   `Hi fellas, I am <p class="text-blue-500 text-3xl sm:text-4xl md:text-5xl"> Mayank Verma</p>`,
   `A Full Stack Developer more specialize in <span class="text-terminal-redText"> Backend Technologies </span> and <span class="text-terminal-redText"> Open Source</span> enthusiast`,
   "passionate about creating innovative solutions using modern technologies.",
-  "Belives in working more than showing",
+  "Believes in working more than showing",
   "Let's Connect to work Together 😊",
 
   "Type 'cd ..' or '..' or click on home to go back",
--- src/components/sections/Skills.tsx
+++ src/components/sections/Skills.tsx
@@ -2,12 +2,14 @@
 
 export const SkillsContent = [
   "Skills:",
-  `• Languages: <span class="text-terminal-redText"> Javascript, Typescript, Java</span>`,
+  `• Languages: <span class="text-terminal-redText"> Javascript, Typescript, Java, Rust</span>`,
   `• Frontend:<span class="text-terminal-redText"> React, Astro docs, Tailwind CSS</span>`,
-  `• Backend: <span class="text-terminal-redText">Node.js, Express</span>`,
+  `• Backend: <span class="text-terminal-redText">Node.js, Express.js, Web-Sockets, Gen-AI</span>`,
   `• Databases:<span class="text-terminal-redText"> MongoDB, PostgreSQL, MySQL</span>`,
   `• Testing: <span class="text-terminal-redText"> Jest, Nightwatch</span>`,
-  `• Tools: <span class="text-terminal-redText"> Git, Github Actions, Docker</span>`,
+  `• Tools: <span class="text-terminal-redText"> Git, Github Actions, Docker, CI/CD</span>`,
+  `• Cloud: <span class="text-terminal-redText"> Amazon Web Services</span>`,
+
   "",
   "Type 'cd ..' or '..' or click on home to go back",
 ];
